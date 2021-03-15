07d4e7788e68bc8dbd7d39ad01dc37522d898eea
JohnMidity <john@office4.in>
2021-03-15 21:46:35 +0530

    unit test

---
+++ O4Desk.Web.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,11 @@
+using Xunit;
+
+namespace O4Desk.Web.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() =>
+            Assert.True(condition: true);
+    }
+}
