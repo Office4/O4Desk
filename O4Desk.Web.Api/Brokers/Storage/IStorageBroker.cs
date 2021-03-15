88c994b4f29de41dd15698426b04f0b189e892d8
JohnMidity <john@office4.in>
2021-03-15 19:23:09 +0530

    Add project files.

---
+++ O4Desk.Web.Api/Brokers/Storage/IStorageBroker.cs
@@ -0,0 +1,6 @@
+﻿namespace O4Desk.Web.Api.Brokers.Storage
+{
+    public interface IStorageBroker
+    {
+    }
+}
---
+++ O4Desk.Web.Api/Brokers/Storage/StorageBroker.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace O4Desk.Web.Api.Brokers.Storage
+{
+    public class StorageBroker : IStorageBroker
+    {
+        private readonly IConfiguration configuration;
+
+        public StorageBroker()
+        {
+
+        }
+    }
+}
