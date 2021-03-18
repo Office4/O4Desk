4fe736d9a0ca8f0cdb980cb27ee94008169c89ce
JohnMidity <john@office4.in>
2021-03-18 19:50:25 +0530

    ShouldCreateCompanyAsync - Fail

---
+++ O4Desk.Web.Api.Tests.Unit/Services/Companies/CompanyServiceTests.Logic.cs
@@ -0,0 +1,52 @@
+﻿using O4Desk.Web.Api.Models.Companies;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using FluentAssertions;
+
+namespace O4Desk.Web.Api.Tests.Unit.Services.Companies
+{
+    public partial class CompanyServiceTests
+    {
+        [Fact]
+        public async Task ShouldCreateCompanyAsync()
+        {
+            // given
+            DateTimeOffset randomDateTime = GetRandomDateTime();
+            DateTimeOffset dateTime = randomDateTime;
+            Company randomCompany = CreateRandomCompany(randomDateTime);
+            randomCompany.UpdatedBy = randomCompany.CreatedBy;
+            Company inputCompany = randomCompany;
+            Company storageCompany = randomCompany;
+            Company expectedCompany = storageCompany;
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime())
+                    .Returns(dateTime);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertCompanyAsync(inputCompany))
+                    .ReturnsAsync(storageCompany);
+
+            // when
+            Company actualCompany =
+                await this.companyService.CreateCompanyAsync(inputCompany);
+
+            // then
+            actualCompany.Should().BeEquivalentTo(expectedCompany);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertCompanyAsync(inputCompany),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ O4Desk.Web.Api.Tests.Unit/Services/Companies/CompanyServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using Moq;
+using O4Desk.Web.Api.Brokers.DateTime;
+using O4Desk.Web.Api.Brokers.Logging;
+using O4Desk.Web.Api.Brokers.Storage;
+using O4Desk.Web.Api.Models.Companies;
+using O4Desk.Web.Api.Services.Companies;
+using System;
+using Tynamix.ObjectFiller;
+
+namespace O4Desk.Web.Api.Tests.Unit.Services.Companies
+{
+    public partial class CompanyServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+
+        private readonly ICompanyService companyService;
+
+        public CompanyServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+
+            this.companyService = new CompanyService(
+                storageBroker: this.storageBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object);
+        }
+
+        private Company CreateRandomCompany() =>
+            CreateCompanyFiller(dates: DateTimeOffset.UtcNow).Create();
+
+        private Company CreateRandomCompany(DateTimeOffset dates) =>
+            CreateCompanyFiller(dates).Create();
+
+        private static Filler<Company> CreateCompanyFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<Company>();
+            Guid createdById = Guid.NewGuid();
+
+            filler.Setup()
+                .OnProperty(company => company.CreatedDate).Use(dates)
+                .OnProperty(company => company.UpdatedDate).Use(dates)
+                .OnProperty(company => company.CreatedBy).Use(createdById)
+                .OnProperty(company => company.UpdatedBy).Use(createdById);
+
+            return filler;
+        }
+
+        private static DateTimeOffset GetRandomDateTime() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+    }
+}
