568c67da30ec05aaecf43a450e832743616ed3eb
JohnMidity <john@office4.in>
2021-05-12 15:29:58 +0530

    ShouldLogWarningOnRetrieveAllWhenCompanysWasEmptyAndLogIt - fail

--- O4Desk.Web.Api.Tests.Unit/Services/Companies/CompanyServiceTests.Validations.Retrieve.cs
+++ O4Desk.Web.Api.Tests.Unit/Services/Companies/CompanyServiceTests.Validations.Retrieve.cs
@@ -1,7 +1,10 @@
-﻿using Moq;
+﻿using FluentAssertions;
+using Moq;
 using O4Desk.Web.Api.Models.Companies;
 using O4Desk.Web.Api.Models.Companies.Exceptions;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -90,6 +93,41 @@ namespace O4Desk.Web.Api.Tests.Unit.Services.Companies
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
 
+        }
+    
+        [Fact]
+        public void ShouldLogWarningOnRetrieveAllWhenCompanysWasEmptyAndLogIt()
+        {
+            // given
+            IQueryable<Company> emptyStorageCompanys = new List<Company>().AsQueryable();
+            IQueryable<Company> expectedCompanys = emptyStorageCompanys;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllCompanies())
+                    .Returns(expectedCompanys);
+
+            // when
+            IQueryable<Company> actualCompanys =
+                this.companyService.RetrieveAllCompanies();
+
+            // then
+            actualCompanys.Should().BeEquivalentTo(emptyStorageCompanys);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogWarning("No companys found in storage."),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllCompanies(),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
