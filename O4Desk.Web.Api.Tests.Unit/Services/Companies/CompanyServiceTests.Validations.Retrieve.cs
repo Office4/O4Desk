0cd5b85a6b8b3645f281de32bc8745fae696ef23
JohnMidity <john@office4.in>
2021-05-12 15:25:48 +0530

    ShouldThrowValidationExceptionOnRetrieveWhenStorageCopmanyIsNullAndLogItAsync - pass

--- O4Desk.Web.Api.Tests.Unit/Services/Companies/CompanyServiceTests.Validations.Retrieve.cs
+++ O4Desk.Web.Api.Tests.Unit/Services/Companies/CompanyServiceTests.Validations.Retrieve.cs
@@ -50,5 +50,46 @@ namespace O4Desk.Web.Api.Tests.Unit.Services.Companies
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
 
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnRetrieveWhenStorageCopmanyIsNullAndLogItAsync()
+        {
+            // given
+            Guid randomCompanyId = Guid.NewGuid();
+            Guid inputCompanyId = randomCompanyId;
+            Company invalidStorageCompany = null;
+            var notFoundCompanyException = new NotFoundCompanyException(inputCompanyId);
+
+            var expectedCompanyValidationException =
+                new CompanyValidationException(notFoundCompanyException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectCompanyByIdAsync(inputCompanyId))
+                    .ReturnsAsync(invalidStorageCompany);
+
+            // when
+            ValueTask<Company> retrieveCompanyByIdTask =
+                this.companyService.RetrieveCompanyByIdAsync(inputCompanyId);
+
+            // then
+            await Assert.ThrowsAsync<CompanyValidationException>(() =>
+                retrieveCompanyByIdTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedCompanyValidationException))),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectCompanyByIdAsync(inputCompanyId),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+
+        }
     }
 }
